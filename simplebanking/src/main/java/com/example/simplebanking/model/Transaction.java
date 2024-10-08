003527c84853ad91c9c02235d2b3c25bdc987de4
cemalgungor <45970194+cemalgungor@users.noreply.github.com>
2024-10-08 14:41:29 +0300

    Update Transaction.java
    MappedSuperclass was removed

--- simplebanking/src/main/java/com/example/simplebanking/model/Transaction.java
+++ simplebanking/src/main/java/com/example/simplebanking/model/Transaction.java
@@ -3,7 +3,6 @@ package com.example.simplebanking.model;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 @Entity
-@MappedSuperclass
 @DiscriminatorColumn(name = "transaction_type")
 public abstract class Transaction {
 
