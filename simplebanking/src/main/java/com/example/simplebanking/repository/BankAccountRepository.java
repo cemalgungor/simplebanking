9a83bc806609cf9053cecc5ae6a0d686876a57f9
cemalgungor <45970194+cemalgungor@users.noreply.github.com>
2024-10-08 14:21:11 +0300

    Update BankAccountRepository.java
    some mistakes removed

--- simplebanking/src/main/java/com/example/simplebanking/repository/BankAccountRepository.java
+++ simplebanking/src/main/java/com/example/simplebanking/repository/BankAccountRepository.java
@@ -1,6 +1,9 @@
 package com.example.simplebanking.repository;
 
 import com.example.simplebanking.model.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
 }
